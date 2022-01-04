cbedf4f538e986126df114429da8627e8a55b109
kpsr363636 <36java36@gmail.com>
2022-01-04 10:45:01 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { EmployeeRegisterComponent } from 'src/employee-register/employee-register.component';
+import { EmployeeAllComponent } from 'src/employee-all/employee-all.component';
+import { EmployeeEditComponent } from 'src/employee-edit/employee-edit.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule,FormsModule,HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule,FormsModule,HttpClientModule,AppRoutingModule],
 
-  declarations: [AppComponent, ],
+  declarations: [AppComponent, EmployeeRegisterComponent, EmployeeAllComponent, EmployeeEditComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
