6b9ecd33cba96cbed0fbeaab9e6977501fbb745f
kpsr363636 <36java36@gmail.com>
2022-01-04 10:18:54 +0530

    4 files modified, tried with environment

--- src/employee.service.ts
+++ src/employee.service.ts
@@ -1,8 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable()
 export class EmployeeService {
-
-  constructor() { }
-
+  baseUri : 'http://localhost:8080/api'
+  constructor() {}
 }
